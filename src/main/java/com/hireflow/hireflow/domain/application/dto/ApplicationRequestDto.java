3e869a39f4772d0b964de68369c82f1e0f7d18e3
juhui <juheelee.dev@gmail.com>
2026-05-08 00:00:18 +0900

    test: add unit tests for AuthService and ApplicationService

--- src/main/java/com/hireflow/hireflow/domain/application/dto/ApplicationRequestDto.java
+++ src/main/java/com/hireflow/hireflow/domain/application/dto/ApplicationRequestDto.java
@@ -1,10 +1,12 @@
 package com.hireflow.hireflow.domain.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ApplicationRequestDto {
 
     private Long jobPostingId; // 어떤 공고에 지원할지
